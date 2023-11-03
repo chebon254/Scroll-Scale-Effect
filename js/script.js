af091d4c2e95a4fee2228c7ad54b71be3513485e
chebon254 <kelvinchebon90@gmail.com>
2023-11-03 13:15:47 +0300

    Code Changes

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ document.addEventListener('DOMContentLoaded', function () {
       const newScrollPosition = window.scrollY;
       const scrollDifference = newScrollPosition - scrollPosition;
 
-      document.getElementById('image1').style.transform = `translateY(${scrollDifference * 0.5}px)`;
-      document.getElementById('image2').style.transform = `translateY(${scrollDifference * 0.8}px)`;
-      document.getElementById('image3').style.transform = `translateY(${scrollDifference * 0.8}px)`;
-      document.getElementById('image4').style.transform = `translateY(${scrollDifference * 0.8}px)`;
-      document.getElementById('image5').style.transform = `translateY(${scrollDifference * 0.8}px)`;
-      document.getElementById('image6').style.transform = `translateY(${scrollDifference * 0.8}px)`;
+      document.getElementById('image1').style.transform = `translateY(${scrollDifference * 1.8}px)`;
+      document.getElementById('image2').style.transform = `translateY(${scrollDifference * 1.8}px)`;
+      document.getElementById('image3').style.transform = `translateY(${scrollDifference * 1.8}px)`;
+      document.getElementById('image4').style.transform = `translateY(${scrollDifference * 1.8}px)`;
+      document.getElementById('image5').style.transform = `translateY(${scrollDifference * 1.8}px)`;
+      document.getElementById('image6').style.transform = `translateY(${scrollDifference * 1.8}px)`;
       // Adjust the multiplier (0.5) to control the scrolling speed
 
       scrollPosition = newScrollPosition;
@@ -18,3 +18,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
     window.addEventListener('scroll', handleScroll);
   });
+
+
+   // Get all elements with the class 'card'
+   const cards = document.querySelectorAll('.card');
+
+   // Function to add the 'hovered' class
+   function addHoverClass() {
+     this.classList.add('card-hover');
+   }
+ 
+   // Function to remove the 'hovered' class
+   function removeHoverClass() {
+     this.classList.remove('card-hover');
+   }
+ 
+   // Add event listeners to each card
+   cards.forEach(card => {
+     card.addEventListener('mouseover', addHoverClass);
+     card.addEventListener('mouseout', removeHoverClass);
+   });
